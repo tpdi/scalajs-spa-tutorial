359e54361a5c1818655e3008be9f498437c55553
TP Diffenbach <tpd@diffenbach.org>
2015-09-16 00:30:56 -0700

    Demonstrate we can create html inline or by calling functions. Demonstrate that Scala's if is an expression. Demonstrate that local functions close over locals.

--- client/src/main/scala/spatutorial/client/components/TodoList.scala
+++ client/src/main/scala/spatutorial/client/components/TodoList.scala
@@ -5,6 +5,8 @@ import japgolly.scalajs.react.vdom.prefix_<^._
 import spatutorial.client.components.Bootstrap.{CommonStyle, Button}
 import spatutorial.shared._
 import scalacss.ScalaCssReact._
+import spatutorial.client.logger._
+import chandu0101.scalajs.react.components.textfields.ReactTagsInput
 
 object TodoList {
   // shorthand for styles
@@ -13,20 +15,28 @@ object TodoList {
   case class TodoListProps(items: Seq[TodoItem], stateChange: (TodoItem) => Unit, editItem: (TodoItem) => Unit, deleteItem: (TodoItem) => Unit)
   
   
-  // I'm "cheating" just a bit: I have no state to mod, but stateChange will send the modification to the server and update all items
+  // I'm "cheating" just a bit: I have no component state to modify, 
+  // but stateChange will send the modification to the server and then update all items and their states
   def updatePriority(item: TodoItem, stateChange: (TodoItem) => Unit)(e: ReactEventI) = {
-      // update TodoItem priority
-      val newPri = e.currentTarget.value match {
-        case p if p == TodoHigh.toString => TodoHigh
-        case p if p == TodoNormal.toString => TodoNormal
-        case p if p == TodoLow.toString => TodoLow
-      }
-      stateChange(item.copy(priority = newPri))
-    }
+      // update TodoItem priority; note that foreach is called on an Option, so it's 0 or 1 at most
+      TodoPriority(e.currentTarget.value).foreach( newPri => stateChange(item.copy(priority = newPri)))
+  }
 
   val TodoList = ReactComponentB[TodoListProps]("TodoList")
     .render(P => {
     val style = bss.listGroup
+    
+    // note that P is captured by the dropDown closure/local function, so we don't need to pass it, or its stateChange, to the function
+    // note also that because the function is one expression, we don't need curly braces
+    def dropDown(item: TodoItem) = if (!item.completed) 
+        <.select(bss.pullRight, ^.value := item.priority.toString, ^.onChange ==> updatePriority(item, P.stateChange),
+              <.option(^.value := TodoHigh.toString, "High"),
+              <.option(^.value := TodoNormal.toString, "Normal"),
+              <.option(^.value := TodoLow.toString, "Low")
+        )
+      else <.span()
+    
+    
     def renderItem(item: TodoItem) = {
       // convert priority into Bootstrap style
       val itemStyle = item.priority match {
@@ -35,18 +45,31 @@ object TodoList {
         case TodoHigh => style.itemOpt(CommonStyle.danger)
       }
       <.li(itemStyle)(
-        <.input(^.tpe := "checkbox", ^.checked := item.completed, ^.onChange --> P.stateChange(item.copy(completed = !item.completed))),
-        <.span(" "),
-        if (item.completed) <.s(item.content) else <.span(item.content),
-        Button(Button.Props(() => P.editItem(item), addStyles = Seq(bss.pullRight, bss.buttonXS)), "Edit"),
-        Button(Button.Props(() => P.deleteItem(item), addStyles = Seq(bss.pullRight, bss.buttonXS)), "Delete"),
-        <.select(bss.pullRight, ^.id := "priority", ^.value := item.priority.toString, ^.onChange ==> updatePriority(item, P.stateChange),
-          <.option(^.value := TodoHigh.toString, "High"),
-          <.option(^.value := TodoNormal.toString, "Normal"),
-          <.option(^.value := TodoLow.toString, "Low")
+        <.input(Seq(bss.pullLeft, GlobalStyles.padRight), ^.tpe := "checkbox", ^.checked := item.completed, 
+            ^.onChange --> P.stateChange(item.copy(completed = !item.completed))),
+        //<.span(" "),
+        //ReactTagsInput(ref = "uncontrolledtags"),
+        <.span(^.onClick ==> ((e: ReactEventH) => { P.editItem(item);}))(if (item.completed) <.s(item.content) else <.span(item.content)),
+        <.div(bss.pullRight)(
+            Button(Button.Props(() => P.editItem(item), addStyles = Seq(bss.pullRight, bss.buttonXS)), "Edit"),
+            Button(Button.Props(() => P.deleteItem(item), addStyles = Seq(bss.pullRight, bss.buttonXS)), "Delete"),
+            
+            // show the priority dropdown only if the item is not completed
+            // inline (scala's if is an expression, not a statement, like the ternary in other languages)...
+            if (!item.completed) 
+              <.select(bss.pullRight, ^.value := item.priority.toString, ^.onChange ==> updatePriority(item, P.stateChange),
+                <.option(^.value := TodoHigh.toString, "High"),
+                <.option(^.value := TodoNormal.toString, "Normal"),
+                <.option(^.value := TodoLow.toString, "Low") 
+              )
+            else 
+              <.span()
+          // or as a helper function if inline becomes too complex
+          , dropDown(item)
         )
       )
     }
+    
     <.ul(style.listGroup)(P.items map renderItem)
   })
     .build
